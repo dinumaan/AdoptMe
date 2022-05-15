b7a6dee6f99553c72a409facd6f0bbbb1a14466f
dinumaan <dineshmatrix675@gmail.com>
2022-05-16 01:26:56 +0530

    added code till handling user input

--- src/SearchParams.js
+++ src/SearchParams.js
@@ -9,7 +9,7 @@ const SearchParams = () => {
   const [animal, setAnimal] = useState("");
   const [breed, setBreed] = useState("");
   const [pets, setPets] = useState([]);
-  const breeds = useBreedList(animal);
+  const [breeds] = useBreedList(animal);
 
   useEffect(() => {
     requestPets();
@@ -25,7 +25,12 @@ const SearchParams = () => {
 
   return (
     <div className="search-params">
-      <form>
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          requestPets();
+        }}
+      >
         <label htmlFor="location">
           Location
           <input
--- src/useBreedList.js
+++ src/useBreedList.js
@@ -1,3 +1,34 @@
+// import { useState, useEffect } from "react";
+
+// const localCache = {};
+
+// export default function useBreedList(animal) {
+//   const [breedList, setBreedList] = useState([]);
+//   const [status, setStatus] = useState("unloaded");
+
+//   useEffect(() => {
+//     if (!animal) {
+//       setBreedList([]);
+//     } else if (localCache[animal]) {
+//       setBreedList(localCache[animal]);
+//     } else {
+//       requestBreedList();
+//     }
+//     async function requestBreedList() {
+//       setBreedList([]);
+//       setStatus("loading");
+//       const res = await fetch(
+//         `http://pets-v2.dev-apis.com/breeds?animal=${animal}`
+//       );
+//       const json = await res.json();
+//       localCache[animal] = json.breeds || [];
+//       setBreedList(localCache[animal]);
+//       setStatus("loaded");
+//     }
+//   }, [animal]);
+//   return [breedList, status];
+// }
+
 import { useState, useEffect } from "react";
 
 const localCache = {};
@@ -14,6 +45,7 @@ export default function useBreedList(animal) {
     } else {
       requestBreedList();
     }
+
     async function requestBreedList() {
       setBreedList([]);
       setStatus("loading");
@@ -26,5 +58,6 @@ export default function useBreedList(animal) {
       setStatus("loaded");
     }
   }, [animal]);
+
   return [breedList, status];
 }
